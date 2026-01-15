ee247cc160f0a7b27d1789f08a79631fdc6909b4
Ivor Turkalj <Ivor.Turkalj@fer.hr>
2026-01-15 21:03:06 +0100

    Popravljen search za sastojke kod dodavanja recepta, osposobljeno dodavanje novih sastojaka u bazu za kreatore

--- IzvorniKod/backend/src/controllers/lookupController.js
+++ IzvorniKod/backend/src/controllers/lookupController.js
@@ -5,12 +5,23 @@ import { AllergenModel } from "../models/Allergen.js";
 export const LookupController = {
   async getIngredients(req, res) {
     try {
-      const data = await IngredientModel.getAll();
+      const { search } = req.query;
+
+      let data;
+
+      if (search && search.trim().length > 0) {
+        data = await IngredientModel.search(search);
+      } else {
+        data = await IngredientModel.getAll();
+      }
+
       res.status(200).json(data);
     } catch (err) {
+      console.error("getIngredients error:", err);
       res.status(500).json({ message: "Greška pri dohvaćanju sastojaka." });
     }
   },
+
 
   async getEquipment(req, res) {
     try {
@@ -29,4 +40,35 @@ export const LookupController = {
       res.status(500).json({ message: "Greška pri dohvaćanju alergena." });
     }
   },
+
+  async createIngredient(req, res) {
+    try {
+      const { name, category } = req.body;
+
+      if (!name || !name.trim()) {
+        return res.status(400).json({ message: "Naziv sastojka je obavezan." });
+      }
+
+      const ingredient = await IngredientModel.create({
+        name: name.trim(),
+        category: category || null,
+      });
+
+      return res.status(201).json(ingredient);
+    } catch (err) {
+      console.error("createIngredient error:", err);
+
+      
+      if (err.code === "23505") {
+        return res.status(409).json({
+          message: "Sastojak s tim imenom već postoji.",
+        });
+      }
+
+      return res.status(500).json({
+        message: "Greška pri dodavanju sastojka.",
+      });
+    }
+  }
+
 };
--- IzvorniKod/backend/src/models/Ingredient.js
+++ IzvorniKod/backend/src/models/Ingredient.js
@@ -7,4 +7,34 @@ export const IngredientModel = {
     );
     return result.rows;
   },
+
+  async search(search) {
+    const result = await pool.query(
+      `
+      SELECT ingredient_id, name, default_unit
+      FROM ingredient
+      WHERE name ILIKE $1
+      ORDER BY name
+      LIMIT 20
+      `,
+      [`%${search}%`]
+    );
+    return result.rows;
+  },
+
+  async create({ name, category }) {
+    const res = await pool.query(
+      `
+      INSERT INTO ingredient (name)
+      VALUES ($1)
+      RETURNING ingredient_id, name, default_unit
+      `,
+      [name]
+    );
+
+    return res.rows[0];
+  },
+
 };
+
+
